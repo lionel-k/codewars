546c243ab8a0e71b38273205bb9145d872e2e9c9
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-13 18:39:10 +0200

    generate pairs

--- 02-sum_pairs/lib/sum_pairs.rb
+++ 02-sum_pairs/lib/sum_pairs.rb
@@ -1,1 +1,26 @@
-def sum_pairs(ints, sum); end
+def sum_pairs(ints, sum)
+  computed_sum = 0
+  pairs = []
+  for i in 0...ints.length
+    for j in (i+1)...ints.length
+      print [i,j]
+    end
+  end
+  # while computed_sum != sum
+  # i = 0
+  # j = 1
+  # while (ints[i] + ints[j]) != sum
+  #   if j <= ints.length - 2
+  #     j += 1
+  #   else
+  #     j += 1
+  #   end
+  # end
+    # puts computed_sum
+    # computed_sum += 1
+  # end
+end
+
+# l1 = [1, 4, 8, 7, 3, 15]
+l1 = [1]*5
+sum_pairs(l1, 8)
