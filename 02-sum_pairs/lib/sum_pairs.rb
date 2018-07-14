f53e3b6abb5ef558f7ad3a5047b7dba50c2949d0
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-14 20:41:50 +0200

    delete useless comments

--- 02-sum_pairs/lib/sum_pairs.rb
+++ 02-sum_pairs/lib/sum_pairs.rb
@@ -3,24 +3,8 @@ def sum_pairs(ints, sum)
   (0...ints.length).step(1) do |i|
     (i + 1...ints.length).step(1) do |j|
       pairs << [i, j]
-      # print pairs
       return [ints[i], ints[j]] if ints[i] + ints[j] == sum
     end
   end
   nil
-
-  # i = 0
-  # while i < pairs.length && (ints[pairs[i][0]] + ints[pairs[i][1]]) != sum
-  #   i += 1
-  # end
-
-  # i == pairs.length ? nil : [ints[pairs[i][0]], ints[pairs[i][1]]]
 end
-
-# l1 = [1, 4, 8, 7, 3, 15]
-# # l1 = [1]*5
-# l3 = [20, -13, 40]
-l5 = [10, 5, 2, 3, 7, 5]
-print sum_pairs(l5, 10)
-# print sum_pairs(l3, -7)
-# sum_pairs(l1, 8)
