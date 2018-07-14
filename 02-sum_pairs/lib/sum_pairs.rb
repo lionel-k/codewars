1b9287bca6ad0bb3a3853c65b20e9c448b106738
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-14 20:43:30 +0200

    useless assignment

--- 02-sum_pairs/lib/sum_pairs.rb
+++ 02-sum_pairs/lib/sum_pairs.rb
@@ -1,8 +1,6 @@
 def sum_pairs(ints, sum)
-  pairs = []
   (0...ints.length).step(1) do |i|
     (i + 1...ints.length).step(1) do |j|
-      pairs << [i, j]
       return [ints[i], ints[j]] if ints[i] + ints[j] == sum
     end
   end
