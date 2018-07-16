31bbecfdc93efc126c390ae7f135dedd06d96913
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-16 10:22:27 +0200

    1st solution

--- 03-array_helpers/lib/array_helpers.rb
+++ 03-array_helpers/lib/array_helpers.rb
@@ -1,8 +1,23 @@
 # Extension of the functionality of the built-in class Array
 class Array
   def square
-    # create your square method here
+    self.map { |n| n * n  }
   end
 
-  # now fill in the rest
+  def cube
+    self.map { |n| n * n * n  }
+  end
+
+  def average
+    self.sum / self.length
+  end
+
+  def even
+    self.select { |e| e.even? }
+  end
+
+  def odd
+    self.select { |e| e.odd? }
+  end
+
 end
