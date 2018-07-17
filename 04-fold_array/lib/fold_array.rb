1595b0eee62cafa188be71fe8f340cfeae0373ca
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-17 11:22:53 +0200

    better style

--- 04-fold_array/lib/fold_array.rb
+++ 04-fold_array/lib/fold_array.rb
@@ -1,15 +1,21 @@
 def split_even_array(array)
   length = array.length
-  [array[0..(length/2)-1],array[(length/2)..(length-1)].reverse]
+  [
+    array[0..(length / 2) - 1],
+    array[(length / 2)..(length - 1)].reverse
+  ]
 end
 
 def split_odd_array(array)
   length = array.length
-  [array[0..(length/2)-1], array[length/2], array[(length/2)+1..(length-1)].reverse]
+  [
+    array[0..(length / 2) - 1], array[length / 2],
+    array[(length / 2) + 1..(length - 1)].reverse
+  ]
 end
 
-def sum_arrays(array_1, array_2)
-  [array_1, array_2].transpose.map {|x| x.reduce(:+)}
+def sum_arrays(array1, array2)
+  [array1, array2].transpose.map { |x| x.reduce(:+) }
 end
 
 def fold_array_once(array)
@@ -17,7 +23,7 @@ def fold_array_once(array)
 
   return array if length == 1
 
-  if (length).even?
+  if length.even?
     splits = split_even_array(array)
     sum_arrays(splits[0], splits[1])
   else
@@ -28,19 +34,19 @@ end
 
 def fold_array(array, runs)
   result = array
-  runs.times do |i|
+  runs.times do
     result = fold_array_once(result)
   end
   result
 end
 
-# input = [ 1, 2, 3, 4, 5, 6, 7 ]
-# input = [ 1, 2, 3, 4, 5, 6 ]
-# input = [ 1, 2, 3, 4, 5 ]
-input = [ 1, 2, 3, 4 ]
-# input = [ 1, 2, 3]
-# input = [ 1, 2]
-# input = [1]
-print fold_array(input, 1)
+# # input = [ 1, 2, 3, 4, 5, 6, 7 ]
+# # input = [ 1, 2, 3, 4, 5, 6 ]
+# # input = [ 1, 2, 3, 4, 5 ]
+# input = [1, 2, 3, 4]
+# # input = [ 1, 2, 3]
+# # input = [ 1, 2]
+# # input = [1]
+# print fold_array(input, 1)
 
 # print [[1,2,3], [4,5,6]].transpose.map {|x| x.reduce(:+)}
