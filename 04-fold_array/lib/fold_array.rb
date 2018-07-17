455fbdf6ec326be3ee74707506199fb182287d46
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-07-17 10:32:13 +0200

    1st attempt

--- 04-fold_array/lib/fold_array.rb
+++ 04-fold_array/lib/fold_array.rb
@@ -1,3 +1,46 @@
-def fold_array(array, runs)
-
+def split_even_array(array)
+  length = array.length
+  [array[0..(length/2)-1],array[(length/2)..(length-1)].reverse]
 end
+
+def split_odd_array(array)
+  length = array.length
+  [array[0..(length/2)-1], array[length/2], array[(length/2)+1..(length-1)].reverse]
+end
+
+def sum_arrays(array_1, array_2)
+  [array_1, array_2].transpose.map {|x| x.reduce(:+)}
+end
+
+def fold_array_once(array)
+  length = array.length
+
+  return array if length == 1
+
+  if (length).even?
+    splits = split_even_array(array)
+    sum_arrays(splits[0], splits[1])
+  else
+    splits = split_odd_array(array)
+    sum_arrays(splits[0], splits[-1]) << splits[1]
+  end
+end
+
+def fold_array(array, runs)
+  result = array
+  runs.times do |i|
+    result = fold_array_once(result)
+  end
+  result
+end
+
+# input = [ 1, 2, 3, 4, 5, 6, 7 ]
+# input = [ 1, 2, 3, 4, 5, 6 ]
+# input = [ 1, 2, 3, 4, 5 ]
+input = [ 1, 2, 3, 4 ]
+# input = [ 1, 2, 3]
+# input = [ 1, 2]
+# input = [1]
+print fold_array(input, 1)
+
+# print [[1,2,3], [4,5,6]].transpose.map {|x| x.reduce(:+)}
--- 04-fold_array/spec/fold_array_spec.rb
+++ 04-fold_array/spec/fold_array_spec.rb
@@ -1,27 +1,27 @@
 require 'fold_array'
 
 describe "Kata Tests" do
-  input = [ 1, 2, 3, 4, 5 ];
+  input_1 = [ 1, 2, 3, 4, 5 ]
 
   it "should pass test#1" do
-    expected = [ 6, 6, 3 ];
-    expect(fold_array(input, 1)).to eq(expected)
+    expected = [ 6, 6, 3 ]
+    expect(fold_array(input_1, 1)).to eq(expected)
   end
 
   it "should pass test#2" do
-    expected = [ 9, 6 ];
-    expect(fold_array(input, 2)).to eq(expected)
+    expected = [ 9, 6 ]
+    expect(fold_array(input_1, 2)).to eq(expected)
   end
 
   it "should pass test#3" do
-    expected = [ 15 ];
-    expect(fold_array(input, 3)).to eq(expected)
+    expected = [ 15 ]
+    expect(fold_array(input_1, 3)).to eq(expected)
   end
 
-  input = [ -9, 9, -8, 8, 66, 23 ];
+  input_2 = [ -9, 9, -8, 8, 66, 23 ]
 
   it "should pass test#4" do
-    expected = [ 14, 75, 0 ];
-    expect(fold_array(input, 1)).to eq(expected);
+    expected = [ 14, 75, 0 ]
+    expect(fold_array(input_2, 1)).to eq(expected)
   end
 end
